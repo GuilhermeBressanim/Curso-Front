016de5cd61b37a41a950112e5cb8b84df32b9ff1
Guilherme Bressanim <guilhermeacb@hotmail.com>
2023-03-04 17:07:25 -0300

    Arrumando o commit anterior

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,20 @@
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
+import { ReactNode } from 'react'
 import CardsSection from '../src/components/homeNoAuth/cardsSection'
 import HeaderNoAuth from '../src/components/homeNoAuth/headerNoAuth'
 import PresentationSection from '../src/components/homeNoAuth/presentationSection'
+import SlideSection from '../src/components/homeNoAuth/slideSection'
+import courseService, { CourseType } from '../src/service/courseService'
 import styles from '../styles/HomeNoAuth.module.scss'
 
-const HomeNoAuth = () => {
+
+interface IndexPageProps {
+  chrildren?: ReactNode
+  course: CourseType[]
+}
+
+const HomeNoAuth = ({ course }: IndexPageProps) => {
   return (
     <>
       <Head>
@@ -19,9 +29,20 @@ const HomeNoAuth = () => {
           <PresentationSection />
         </div>
         <CardsSection />
+        <SlideSection newestCourses={course} />
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await courseService.getNewestCourses()
+  return {
+    props: {
+      course: res.data
+    },
+    revalidate: 3600 * 24
+  }
 }
 
 export default HomeNoAuth
