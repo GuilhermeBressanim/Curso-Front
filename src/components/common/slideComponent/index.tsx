60a02c50755e9707295407aaaf7ef68790f33d03
Guilherme Bressanim <guilhermeacb@hotmail.com>
2023-03-04 16:55:36 -0300

    Finalizada seção dos slides, visual e também responsividade - Seção dos slides

--- src/components/common/slideComponent/index.tsx
+++ src/components/common/slideComponent/index.tsx
@@ -8,14 +8,42 @@ interface props {
 }
 
 const SlideComponent = function ({ course }: props) {
+    let slideCount = 0
+
+    if (course.length > 4) {
+        slideCount = 4
+    } else if (course) {
+        slideCount = course.length
+    }
+
     return (
         <>
-            <div>
+            <div className='d-flex flex-column align-items-center py-4'>
                 <Splide options={{
                     type: "loop",
-                    perPage: 4,
+                    perPage: slideCount,
                     perMove: 1,
-                    pagination: false
+                    width: slideCount * 300,
+                    pagination: false,
+                    arrows: course.length > 4 ? true : false,
+                    drag: course.length > 4 ? true : false,
+                    breakpoints: {
+                        1200: {
+                            perPage: slideCount >= 2 ? 2 : 1,
+                            width: slideCount >= 2 ? 600 : 300,
+                            arrows: course.length > 2 ? true : false,
+                            drag: course.length > 2 ? true : false,
+                        },
+                        600: {
+                            perPage: 1,
+                            width: 300
+                            arrows: course.length > 1 ? true : false,
+                            drag: course.length > 1 ? true : false,
+                        },
+                        300: {
+                            width: 250
+                        }
+                    }
                 }}>
                     {course?.map((course) => (
                         <SplideSlide key={course.id}>
--- src/components/homeNoAuth/slideSection/index.tsx
+++ src/components/homeNoAuth/slideSection/index.tsx
@@ -12,13 +12,13 @@ interface props {
 const SlideSection = function ({ newestCourses }: props) {
     return (
         <>
-            <Container>
-                <p className={styles.sectionTitle}>AULAS JÁ DISPONIVEIS</p>
+            <Container className="d-flex flex-column align-items-center py-5">
+                <p className={styles.sectionTitle}>AULAS JÁ DISPONÍVEIS</p>
                 <SlideComponent course={newestCourses} />
+                <Link href="/register">
+                    <Button outline color="light" className={styles.SlideSectionBtn}>Se cadastre para acessar!</Button>
+                </Link>
             </Container>
-            <Link href="/register">
-                <Button outline color="light" className={styles.SlideSectionBtn}>Se cadastre para acessar!</Button>
-            </Link>
         </>
     )
 }
