7b96b945e93c27f66c4987e6235dfad7f066594f
Guilherme Bressanim <guilhermeacb@hotmail.com>
2023-03-15 14:28:54 -0300

    Configuração finalizada da nossa seção de destaque. Feita conexão com o backend, criando um método para o serviço de curso. Criando a sua responsividade - Seção destaque

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head"
-import HeaderAuth from "../src/components/common/headerAuth"
+import FeaturedSection from "../src/components/homeAuth/featuredSection"
 
 const HomeAuth = function () {
     return (
@@ -9,7 +9,7 @@ const HomeAuth = function () {
                 <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
             </Head>
             <main>
-                <HeaderAuth></HeaderAuth>
+                <FeaturedSection />
             </main>
         </>
     )
---
+++ src/components/homeAuth/featuredSection/index.tsx
@@ -0,0 +1,44 @@
+import styles from "./styles.module.scss"
+import useSWR from "swr"
+import courseService, { CourseType } from "../../../service/courseService"
+import HeaderAuth from "../../common/headerAuth"
+import { Container, Button } from "reactstrap"
+import Link from "next/link"
+
+const FeaturedSection = function () {
+    const { data, error } = useSWR("/featured", courseService.getFeaturedCourses)
+
+    if (error) return error
+    if (!data) return (<><p>Loading...</p></>)
+
+    return (
+        <>
+            {
+                data.data?.map((course: CourseType) => (
+                    <div style={{
+                        backgroundImage: `linear-gradient(to bottom, #6666661a, #151515), url(
+                        ${process.env.NEXT_PUBLIC_BASEURL}/${course.thumbnailUrl})`,
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                        height: "480px"
+                    }}
+                        key={course.id}>
+                        <HeaderAuth />
+                        <Container className="pt-4">
+                            <p className={styles.title}>{course.name}</p>
+                            <p className={styles.description}>{course.synopsis}</p>
+                            <Link href={`/courses/${course.id}`}>
+                                <Button outline color="light" className={styles.button}>
+                                    ACESSE AGORA
+                                    <img src="/buttonPlay.svg" alt="buttonImg" className={styles.buttonImg} />
+                                </Button>
+                            </Link>
+                        </Container>
+                    </div >
+                ))[0]
+            }
+        </>
+    )
+}
+
+export default FeaturedSection
