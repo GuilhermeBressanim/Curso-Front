f48d8188349cfcf84cbbd77986fd2699418733b1
Guilherme Bressanim <guilhermeacb@hotmail.com>
2023-03-19 12:54:58 -0300

    Conexão do backend relacionado ao update de senha. Criação do método e também de toda a configuração de conexão com o frontend - Profile (password - conexão com o backend)

--- src/components/profile/password/index.tsx
+++ src/components/profile/password/index.tsx
@@ -1,10 +1,80 @@
+import { FormEvent, useEffect, useState } from "react"
 import { Button, Form, FormGroup, Input, Label } from "reactstrap"
 import styles from "../../../../styles/profile.module.scss"
+import profileService from "../../../service/profileService"
+import ToastComponent from "../../common/toast"
 
 const PasswordForm = function () {
+    const [currentPassword, setCurrentPassword] = useState("");
+    const [newPassword, setNewPassword] = useState("");
+    const [confirmNewPassword, setConfirmNewPassword] = useState("");
+    const [color, setColor] = useState("")
+    const [toastIsOpen, setToastIsOpen] = useState(false)
+    const [errorMessage, setErrorMessage] = useState("")
+
+    useEffect(() => {
+        profileService.fetchCurrent().then((password) => {
+            setCurrentPassword(password.currentPassword);
+            setNewPassword(password.newPassword);
+        });
+    }, []);
+
+    const handlePasswordUpdate = async function (event: FormEvent<HTMLFormElement>) {
+        event.preventDefault()
+
+        if (newPassword != confirmNewPassword) {
+            setToastIsOpen(true)
+            setErrorMessage("Senha e confirmação de senha diferentes!")
+            setColor("bg-danger")
+            setTimeout(() => {
+                setToastIsOpen(false)
+            }, 1000 * 3)
+
+            return
+        }
+
+
+        if (currentPassword === newPassword) {
+            setToastIsOpen(true)
+            setErrorMessage("Senha nova e senha atual são iguais!")
+            setColor("bg-danger")
+            setTimeout(() => {
+                setToastIsOpen(false)
+            }, 1000 * 3)
+
+            return
+        }
+
+        const res = await profileService.passwordUpdate({
+            currentPassword, newPassword
+        })
+
+        if (res === 204) {
+            setToastIsOpen(true)
+            setErrorMessage("Senha alterada com sucesso!")
+            setColor("bg-success")
+            setTimeout(() => {
+                setToastIsOpen(false)
+            }, 1000 * 3)
+
+            setCurrentPassword("")
+            setNewPassword("")
+            setConfirmNewPassword("")
+        }
+
+        if (res === 400) {
+            setToastIsOpen(true)
+            setErrorMessage("Senha atual incorreta!")
+            setColor("bg-danger")
+            setTimeout(() => {
+                setToastIsOpen(false)
+            }, 1000 * 3)
+        }
+    }
+
     return (
         <>
-            <Form className={styles.form}>
+            <Form onSubmit={handlePasswordUpdate} className={styles.form}>
                 <div className={styles.inputNormalDiv}>
                     <FormGroup>
                         <Label className={styles.label} for="currentPassword">SENHA ATUAL</Label>
@@ -16,6 +86,10 @@ const PasswordForm = function () {
                             required
                             minLength={6}
                             maxLength={12}
+                            value={currentPassword}
+                            onChange={(event) => {
+                                setCurrentPassword(event.currentTarget.value)
+                            }}
                             className={styles.input}
                         />
                     </FormGroup>
@@ -33,6 +107,10 @@ const PasswordForm = function () {
                             required
                             minLength={6}
                             maxLength={12}
+                            value={newPassword}
+                            onChange={(event) => {
+                                setNewPassword(event.currentTarget.value)
+                            }}
                             className={styles.inputFlex}
                         />
                     </FormGroup>
@@ -48,15 +126,20 @@ const PasswordForm = function () {
                             required
                             minLength={6}
                             maxLength={12}
+                            value={confirmNewPassword}
+                            onChange={(event) => {
+                                setConfirmNewPassword(event.currentTarget.value)
+                            }}
                             className={styles.inputFlex}
                         />
                     </FormGroup>
                 </div>
 
-                <Button className={styles.formBtn} outline>
+                <Button type="submit" className={styles.formBtn} outline>
                     SALVAR ALTERAÇÕES
                 </Button>
             </Form>
+            <ToastComponent color={color} isOpen={toastIsOpen} message={errorMessage} />
         </>
     )
 }
--- src/service/profileService.ts
+++ src/service/profileService.ts
@@ -8,6 +8,11 @@ interface UserParams {
     created_at: string
 }
 
+interface PasswordParams {
+    currentPassword: string
+    newPassword: string
+}
+
 const profileService = {
     fetchCurrent: async () => {
         const token = sessionStorage.getItem("onebitflix-token")
@@ -27,6 +32,23 @@ const profileService = {
         const token = sessionStorage.getItem("onebitflix-token")
 
         const res = await api.put("/users/current", params, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).catch(error => {
+            if (error.response.status === 400 || error.response.status === 401) {
+                return error.response
+            }
+            return error.response
+        })
+
+        return res.status
+    },
+
+    passwordUpdate: async (params: PasswordParams) => {
+        const token = sessionStorage.getItem("onebitflix-token")
+
+        const res = await api.put("/users/current/password", params, {
             headers: {
                 Authorization: `Bearer ${token}`
             }
