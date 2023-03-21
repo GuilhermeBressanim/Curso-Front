335d3385e022c50b59f75305fcc5f4c1b9934d7d
Guilherme Bressanim <guilhermeacb@hotmail.com>
2023-03-21 20:37:59 -0300

    Criação do método de tempo para a página de episódios, criação do seu service e também conexão com o front - Página de episódio

--- pages/course/episode/[id].tsx
+++ pages/course/episode/[id].tsx
@@ -2,17 +2,53 @@ import styles from "../../../styles/episodePlayer.module.scss";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import HeaderGeneric from "../../../src/components/common/headerGeneric";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import courseService, { CourseType } from "../../../src/service/courseService";
 import PageSpinner from "../../../src/components/common/spinner";
 import { Button, Container } from "reactstrap";
 import ReactPlayer from "react-player";
+import watchEpisodeService from "../../../src/service/episodeService";
 
 const EpisodePlayer = function () {
     const router = useRouter()
     const [course, setCourse] = useState<CourseType>()
+    const [isReady, setIsReady] = useState(false)
     const episodeOrder = parseFloat(router.query.id?.toString() || "")
+    const episodeId = parseFloat(router.query.episodeid?.toString() || "")
     const courseId = router.query.courseid?.toString() || ""
+    const [getEpisodeTime, setGetEpisodeTime] = useState(0)
+    const [episodeTime, setEpisodeTime] = useState(0)
+    const playerRef = useRef<ReactPlayer>(null)
+
+    const handleGetEpisodeTime = async () => {
+        const res = await watchEpisodeService.getWatchTime(episodeId)
+
+        if (res.data !== null) {
+            setGetEpisodeTime(res.data.seconds)
+        }
+    }
+
+    const handleSetEpisodeTime = async () => {
+        await watchEpisodeService.setWatchTime({
+            episodeId: episodeId,
+            seconds: Math.round(episodeTime)
+        })
+    }
+
+    useEffect(() => {
+        handleGetEpisodeTime()
+    }, [router])
+
+    const handlePlayerTime = () => {
+        playerRef.current?.seekTo(getEpisodeTime)
+        setIsReady(true)
+    }
+
+    if (isReady === true) {
+        setTimeout(() => {
+            handleSetEpisodeTime()
+        }, 1000 * 3)
+    }
 
     const getCourse = async function () {
         if (typeof courseId !== "string") return
@@ -24,12 +60,16 @@ const EpisodePlayer = function () {
         }
     }
 
+
+
     const handleLastEpisode = () => {
-        router.push(`/course/episode/${episodeOrder - 1}?courseid=${course?.id}`)
+        router.push(`/course/episode/${episodeOrder - 1}?courseid=${course?.id}&episodeid=${episodeId - 1
+            }`)
     }
 
     const handleNextEpisode = () => {
-        router.push(`/course/episode/${episodeOrder + 1}?courseid=${course?.id}`)
+        router.push(`/course/episode/${episodeOrder + 1}?courseid=${course?.id}&episodeid=${episodeId + 1
+            }`)
     }
 
     useEffect(() => {
@@ -37,6 +77,12 @@ const EpisodePlayer = function () {
     }, [courseId])
 
     if (course?.episodes === undefined) return <PageSpinner />
+
+    if (episodeOrder + 1 < course?.episodes?.length) {
+        if (Math.round(episodeTime) === course.episodes[episodeOrder].secondsLong) {
+            handleNextEpisode()
+        }
+    }
 
     return (
         <>
@@ -52,7 +98,13 @@ const EpisodePlayer = function () {
                     </p>
                     {typeof window === 'undefined' ? null : (
                         <ReactPlayer className={styles.player}
-                            url={`${process.env.NEXT_PUBLIC_BASEURL}/episodes/stream?videoUrl=${course.episodes[episodeOrder].videoUrl}&token=${sessionStorage.getItem('onebitflix-token')}`} controls />
+                            url={`${process.env.NEXT_PUBLIC_BASEURL}/episodes/stream?videoUrl=${course.episodes[episodeOrder].videoUrl}&token=${sessionStorage.getItem('onebitflix-token')}`} controls
+                            ref={playerRef}
+                            onStart={handlePlayerTime}
+                            onProgress={(progress) => {
+                                setEpisodeTime(progress.playedSeconds)
+                            }}
+                        />
                     )}
                     <div className={styles.episodeButtonDiv}>
                         <Button className={styles.episodeButton}
